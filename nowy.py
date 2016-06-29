7cc192c0417f6c7f58af91f55eef122aa96fbddc
nowy <nowy@wp.pl>
2016-06-29 14:30:42 +0200

    ver2

--- nowy.py
+++ nowy.py
@@ -5,21 +5,20 @@
 import random
 
 twojeliczby = []
-liczby =[]
+liczby = []
+
 
 
 for i in range(6):
     
+       
+    
     twojaliczba = int(raw_input("Podaj twoja liczbe " + str(i+1) + " : "))
-    if twojaliczba > 1 and twojaliczba <49:
-        if twojeliczby.count(twojaliczba)==0:
-            twojeliczby.append(twojaliczba)
-        else: 
-            print "Ta liczba juz byla!! ( Podaj inna)"
-           # i = i + 1
-    else:
-         print "Zly zakres!!!"
-         # i = i + 1
+
+    #if twojaliczba > 1 and twojaliczba <49:
+    if twojeliczby.count(twojaliczba)==0:
+       twojeliczby.append(twojaliczba)
+      
 
 print "Twoje liczby", twojeliczby
 
@@ -28,8 +27,15 @@ for i in range(6):
 
     liczba = random.randint(1,49)
     if liczby.count(liczba)==0:
-        liczby.append(liczba)
-
+       liczby.append(liczba)      
+    
 
 print "wylosowane liczby : ", liczby
+
+
+trafione = set(twojeliczby) & set(liczby)
+
+print "trafione : ", trafione
+print "trafione : ", len(trafione)
+
 

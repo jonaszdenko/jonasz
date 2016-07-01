b92c3737d4079863345b2c01db1c3ff7ce61438c
nowy <nowy@wp.pl>
2016-07-01 14:39:21 +0200

    Ver2

--- nowymoduly.py
+++ nowymoduly.py
@@ -1,11 +1,44 @@
 #! /usr/bin/env python
 
-
-print "TEST"
-
 import random
+
+def twojeliczby():
+
+ 
 	
 
+    for i in range(6):
+    
+  
+
+      while True:
+    
+        j=0
+   
+        try: 
+            twojaliczba = int(raw_input("Podaj twoja liczbe " + str(i+1) + " : "))
+
+            if twojaliczba < 1 or twojaliczba > 49:
+               print "Bledne dane!!!"
+               j=1
+           
+
+            else:
+              if twojeliczby.count(twojaliczba)==0:
+                  twojeliczby.append(twojaliczba)
+              else:
+                  print "Ta liczba juz byla!"
+                  j=1
+
+          
+        except:
+             print "zla podana wartosc"
+             j=1
+
+        if j==0:
+          break
+
+    return(twojeliczby)
 
 
 

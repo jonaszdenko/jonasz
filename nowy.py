f9d446c395b31a21e664e12d29cc67cb504f4728
jonaszdenko <denko.jonasz@gmail.com>
2016-07-03 19:44:13 +0200

    Rozbite na moduly 3.07

--- nowy.py
+++ nowy.py
@@ -1,54 +1,17 @@
 #! /usr/bin/env python
 
+#from nowymoduly import twojeliczbyfun, losowaneliczbyfun
 
-
-import random
+import nowymoduly
 
 twojeliczby = []
 liczby = []
 
 
-
-for i in range(6):
-    
-  
-
-  while True:
-    
-    j=0
-   
-    try: 
-        twojaliczba = int(raw_input("Podaj twoja liczbe " + str(i+1) + " : "))
-
-        if twojaliczba < 1 or twojaliczba > 49:
-           print "Bledne dane!!!"
-           j=1
-           
-
-        else:
-          if twojeliczby.count(twojaliczba)==0:
-              twojeliczby.append(twojaliczba)
-          else:
-              print "Ta liczba juz byla!"
-              j=1
-
-          
-    except:
-         print "zla podana wartosc"
-         j=1
-
-    if j==0:
-      break
+twojeliczby = nowymoduly.twojeliczbyfun()
+liczby = nowymoduly.losowaneliczbyfun()
 
 print "Twoje liczby", twojeliczby
-
-for i in range(6):
-
-       liczba = random.randint(1,49)
-       if liczby.count(liczba)==0:
-          liczby.append(liczba)      
-    
-
 print "wylosowane liczby : ", liczby
 
 
--- nowymoduly.py
+++ nowymoduly.py
@@ -2,18 +2,18 @@
 
 import random
 
-def twojeliczby():
+#funkcja w ktorej podajemy nasze liczby. Szesc liczb w petli for.
+#Kolejno obsulga wyjatkow (while: co ma robic, try:jesli jakis wyjatek na wejsciu to co robic. Kolejno jesli liczba ze zlego zakresu i kiedy liczba sie powtarza
+def twojeliczbyfun():
 
+    twojeliczby =[]
  
-	
 
     for i in range(6):
     
-  
-
       while True:
     
-        j=0
+        j=0#jesli to znaczy ze udalo sie dodac liczbe, j zmienia swoj stan jesli cos jest nie tak ( dziala to na zasadzie do...while z tym. 
    
         try: 
             twojaliczba = int(raw_input("Podaj twoja liczbe " + str(i+1) + " : "))
@@ -24,8 +24,8 @@ def twojeliczby():
            
 
             else:
-              if twojeliczby.count(twojaliczba)==0:
-                  twojeliczby.append(twojaliczba)
+              if twojeliczby.count(twojaliczba)==0:#liczy ilosc liczb w zbiorze liczby jesli jest ich 0 to idzie dalej
+                  twojeliczby.append(twojaliczba)#dodaeje do zbioru
               else:
                   print "Ta liczba juz byla!"
                   j=1
@@ -43,3 +43,26 @@ def twojeliczby():
 
 
 
+def losowaneliczbyfun():
+ 
+    losowane = []
+
+    for i in range(6):
+        
+       while True:
+            
+           j=0
+
+           liczba = random.randint(1,49)
+           if losowane.count(liczba)==0:
+              losowane.append(liczba)      
+           else:
+            j=1
+          
+           if j==0:
+              break
+
+        
+
+       
+    return(losowane)
